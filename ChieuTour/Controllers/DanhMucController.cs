311331c3d555f545cc279eaf2b081c8cb1c332ec
hkgofk <nguyentrunghieu9029@gmail.com>
2022-04-14 14:03:54 +0700

    add layout client

---
+++ ChieuTour/Controllers/DanhMucController.cs
@@ -0,0 +1,28 @@
+﻿using ChieuTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChieuTour.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        private ChieuTourDBContext db = new ChieuTourDBContext();
+        // GET: DanhMuc
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult ListDanhMuc()
+        {
+            var danhMucs = db.DanhMucs.ToList();
+            ViewBag.danhMucTour = danhMucs;
+
+            return PartialView("_DanhMuc");
+        }
+
+    }
+}
