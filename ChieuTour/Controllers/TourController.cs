c7e888b706b43e5c368bbaa0e00c37e656fa790c
hkgofk <nguyentrunghieu9029@gmail.com>
2022-04-14 16:26:57 +0700

    code tour detail

---
+++ ChieuTour/Controllers/TourController.cs
@@ -0,0 +1,21 @@
+﻿using ChieuTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChieuTour.Controllers
+{
+    public class TourController : Controller
+    {
+        private ChieuTourDBContext db = new ChieuTourDBContext();
+        // GET: Tour
+        public ActionResult Detail(int id)
+        {
+            Tour tour = db.Tours.Find(id);
+
+            return View(tour);
+        }
+    }
+}
