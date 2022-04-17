3b464b1a55933c05c08465528f9a69d65bd6c66c
hkgofk <nguyentrunghieu9029@gmail.com>
2022-04-17 09:59:46 +0700

    login logout customer

---
+++ ChieuTour/Controllers/DangKyController.cs
@@ -0,0 +1,75 @@
+﻿using ChieuTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChieuTour.Controllers
+{
+    public class DangKyController : Controller
+    {
+        private ChieuTourDBContext db = new ChieuTourDBContext();
+        // GET: DangKy
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection collection, NguoiDung kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhaunhaplai = collection["Matkhaunhaplai"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (string.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (string.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (string.IsNullOrEmpty(matkhaunhaplai))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (string.IsNullOrEmpty(diachi))
+            {
+                ViewData["Loi5"] = "Địa chỉ không được để trống";
+            }
+            else if (string.IsNullOrEmpty(email))
+            {
+                ViewData["Loi6"] = "Email không được để trống";
+            }
+            else if (string.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi7"] = "Phải nhập số điện thoại";
+            }
+
+            else
+            {
+                kh.HoTen = hoten;
+                kh.TaiKhoan = tendn;
+                kh.MatKhau = matkhau;
+                kh.Email = email;
+                kh.DiaChi = diachi;
+                kh.SDT = dienthoai;
+                kh.IdQuyen = 2;
+                db.NguoiDungs.Add(kh);
+                db.SaveChanges();
+                TempData["success"] = "Đăng ký thành công!";
+            }
+            return this.Index();
+        }
+    }
+}
---
+++ ChieuTour/Controllers/NguoiDungController.cs
@@ -0,0 +1,51 @@
+﻿using ChieuTour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChieuTour.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        private ChieuTourDBContext db = new ChieuTourDBContext();
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                NguoiDung kh = db.NguoiDungs.SingleOrDefault(n => n.TaiKhoan == tendn && n.MatKhau == matkhau);
+                if (kh != null)
+                {
+                    TempData["success"] = "Chúc mứng đăng nhập thành công";
+                    Session["Tendangnhap"] = kh.HoTen;
+                    return RedirectToAction("Index", "Home");
+                }
+                else TempData["error"] = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+        public ActionResult logout()
+        {
+            Session["Tendangnhap"] = null;
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
