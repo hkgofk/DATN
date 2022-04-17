4a2ca3a50a99e180b4eb7b5fb553fbbbe3b9bff7
hkgofk <nguyentrunghieu9029@gmail.com>
2022-04-17 21:54:16 +0700

    confirm don hang

---
+++ ChieuTour/Areas/Admin/Controllers/DonHangController.cs
@@ -0,0 +1,59 @@
+﻿using ChieuTour.Models;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChieuTour.Areas.Admin.Controllers
+{
+    public class DonHangController : Controller
+    {
+        private ChieuTourDBContext db = new ChieuTourDBContext();
+        // GET: Admin/DonHang
+        public ActionResult Index()
+        {
+            var donHang = db.DonHangs.Include(d => d.NguoiDung);
+
+            return View(donHang.ToList());
+        }
+        public ActionResult Detail(int MaDon)
+        {
+            List<NguoiDung> nguoidung = db.NguoiDungs.ToList();
+            List<ChiTietDonHang> ctdh = db.ChiTietDonHangs.ToList();
+            List<DonHang> donhang = db.DonHangs.ToList();
+            List<Tour> tour = db.Tours.ToList();
+            var main = from d in donhang
+                       join n in nguoidung on d.MaNguoiDung equals n.MaNguoiDung
+                       where (d.MaDon == MaDon)
+                       select new ViewModel
+                       {
+                           donhang = d,
+                           nguoidung = n
+                       };
+            var sub = ctdh.Where(c => c.MaDon == MaDon).Join(tour, c => c.MaTour, t => t.MaTour, (c, t) => new ViewModel
+            {
+                ctdh = c,
+                tour = t,
+                thanhTien = c.SoLuongNL * (float)t.GiaNL * (1 - (float)t.GiamGia/100) + c.SoLuongTE * (float)t.GiaTE * (1 - (float)t.GiamGia / 100)
+            });
+            ViewBag.Main = main;
+            ViewBag.Sub = sub;
+            ViewBag.maDon = MaDon;
+
+            return View();
+        }
+        public ActionResult XacNhan(int MaDon, string text)
+        {
+            var donHang = db.DonHangs.Where(d => d.MaDon == MaDon).FirstOrDefault();
+            donHang.TinhTrang = text;
+            db.Entry(donHang).State = EntityState.Modified;
+            db.SaveChanges();
+            TempData["success"] = "Cập nhật thành công!";
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ ChieuTour/Models/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChieuTour.Models
+{
+    public class ViewModel
+    {
+        public NguoiDung nguoidung { get; set; }
+        public ChiTietDonHang ctdh { get; set; }
+        public DonHang donhang { get; set; }
+        public Tour tour { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.##0}")]
+        public double thanhTien { get; set; }
+    }
+}
