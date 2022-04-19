8b0ed521f61e1ae9fdc0bb3d5205dc85a1684c2c
hkgofk <nguyentrunghieu9029@gmail.com>
2022-04-19 16:40:09 +0700

    custom layout

--- ChieuTour/Controllers/HomeController.cs
+++ ChieuTour/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChieuTour.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,26 @@ namespace ChieuTour.Controllers
 {
     public class HomeController : Controller
     {
+        private ChieuTourDBContext db = new ChieuTourDBContext();
         public ActionResult Index()
         {
+            var kq = (from k in db.Tours
+                      let tongsl = (from c in db.ChiTietDonHangs
+                                    join t in db.Tours
+                                    on c.MaTour equals t.MaTour
+                                    where c.MaTour == k.MaTour
+                                    select c.SoLuongTE + c.SoLuongNL).Sum()
+                      orderby tongsl descending
+                      select new ViewModel
+                      {
+                          tour = k
+                      }).Take(6);
+
+            ViewBag.top6 = kq;
+            var listTT = db.TinTucs.Select(t => t).OrderBy(t => t.NgayDang).Take(12);
+            ViewBag.TT = listTT;
             return View();
+
         }
 
         public ActionResult About()
