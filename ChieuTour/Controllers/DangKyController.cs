6ae78047cd65771967bc5b4ad8ad4d1f1e311044
hkgofk <nguyentrunghieu9029@gmail.com>
2022-04-23 15:18:04 +0700

    fix dang ky tai khoan

--- ChieuTour/Controllers/DangKyController.cs
+++ ChieuTour/Controllers/DangKyController.cs
@@ -27,49 +27,57 @@ namespace ChieuTour.Controllers
             var email = collection["Email"];
             var dienthoai = collection["Dienthoai"];
 
-            if (String.IsNullOrEmpty(hoten))
+            try
             {
-                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
-            }
-            else if (string.IsNullOrEmpty(tendn))
-            {
-                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
-            }
-            else if (string.IsNullOrEmpty(matkhau))
-            {
-                ViewData["Loi3"] = "Phải nhập mật khẩu";
-            }
-            else if (string.IsNullOrEmpty(matkhaunhaplai))
-            {
-                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
-            }
-            else if (string.IsNullOrEmpty(diachi))
-            {
-                ViewData["Loi5"] = "Địa chỉ không được để trống";
-            }
-            else if (string.IsNullOrEmpty(email))
-            {
-                ViewData["Loi6"] = "Email không được để trống";
-            }
-            else if (string.IsNullOrEmpty(dienthoai))
-            {
-                ViewData["Loi7"] = "Phải nhập số điện thoại";
-            }
+                if (String.IsNullOrEmpty(hoten))
+                {
+                    ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+                }
+                else if (string.IsNullOrEmpty(tendn))
+                {
+                    ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+                }
+                else if (string.IsNullOrEmpty(matkhau))
+                {
+                    ViewData["Loi3"] = "Phải nhập mật khẩu";
+                }
+                else if (string.IsNullOrEmpty(matkhaunhaplai))
+                {
+                    ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+                }
+                else if (string.IsNullOrEmpty(diachi))
+                {
+                    ViewData["Loi5"] = "Địa chỉ không được để trống";
+                }
+                else if (string.IsNullOrEmpty(email))
+                {
+                    ViewData["Loi6"] = "Email không được để trống";
+                }
+                else if (string.IsNullOrEmpty(dienthoai))
+                {
+                    ViewData["Loi7"] = "Phải nhập số điện thoại";
+                }
 
-            else
-            {
-                kh.HoTen = hoten;
-                kh.TaiKhoan = tendn;
-                kh.MatKhau = matkhau;
-                kh.Email = email;
-                kh.DiaChi = diachi;
-                kh.SDT = dienthoai;
-                kh.IdQuyen = 2;
-                db.NguoiDungs.Add(kh);
-                db.SaveChanges();
-                TempData["success"] = "Đăng ký thành công!";
+                else
+                {
+                    kh.HoTen = hoten;
+                    kh.TaiKhoan = tendn;
+                    kh.MatKhau = matkhau;
+                    kh.Email = email;
+                    kh.DiaChi = diachi;
+                    kh.SDT = dienthoai;
+                    kh.IdQuyen = 2;
+                    db.NguoiDungs.Add(kh);
+                    db.SaveChanges();
+                    TempData["success"] = "Đăng ký thành công!";
+                }
+                return RedirectToAction("Index", "NguoiDung");
             }
-            return RedirectToAction("Index", "NguoiDung");
+            catch (Exception)
+            {
+                TempData["error"] = "Đăng ký không thành công!";
+            }
+            return View();
         }
     }
 }
